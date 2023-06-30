252966fdd583fe6b21f17d22da7339673cd92792
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2023-06-30 13:25:22 +0300

    redesigned bot shutdown when the websocket client is disabled

--- backend/app/bot.py
+++ backend/app/bot.py
@@ -1,18 +1,26 @@
-from typing import Callable
+import asyncio
+
+from fastapi import WebSocket
+from loguru import logger
 
 from twitchio.ext import commands
+from websockets.exceptions import WebSocketException
 
 
 class TwitchBot(commands.Bot):
-    def __init__(self, token, channel, message_callback: Callable):
+    def __init__(self, token, channel, websocket: WebSocket):
         super().__init__(token=token, prefix="", initial_channels=[channel])
 
-        self.message_callback: Callable = message_callback
+        self._websocket: WebSocket = websocket
 
     async def event_message(self, message):
         if message.echo:
             return
+        try:
+            if message.content == 'plus':
+                await self._websocket.send_text('plus')
+            elif message.content == 'minus':
+                await self._websocket.send_text('minus')
 
-        await self.message_callback(message)
-
-        await self.handle_commands(message)
+        except Exception as e:
+            await self.close()
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, WebSocket
 from loguru import logger
-from starlette.websockets import WebSocketDisconnect
 
 from app.bot import TwitchBot
 from app.twitch import get_user_info
@@ -10,22 +9,23 @@ router: APIRouter = APIRouter()
 
 @router.websocket("/ws")
 async def websocket_endpoint(access_token: str, websocket: WebSocket):
-    twitch_user = await get_user_info(access_token)
+    try:
+        twitch_user = await get_user_info(access_token)
+    except Exception as e:
+        logger.error(e)
+        # await websocket.close(1000)
+        return
+
     channel_name = twitch_user['login']
 
     logger.info(f'connection request from user {twitch_user}')
 
-    async def _callback(message):
-        if message.content == 'plus':
-            await websocket.send_text('plus')
-        elif message.content == 'minus':
-            await websocket.send_text('minus')
-
-    bot = TwitchBot(token=access_token, channel=channel_name, message_callback=_callback)
+    bot = TwitchBot(token=access_token, channel=channel_name, websocket=websocket)
 
     try:
         await websocket.accept()
         await bot.start()
-    except WebSocketDisconnect:
-        logger.info(f'connection closed from user {twitch_user}')
-        await bot.close()
+        while True:
+            await websocket.receive()
+    except Exception as e:
+        logger.info(f'socket was closed: {e}')
