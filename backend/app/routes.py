e6a46333c30a3ae7d09fff75ad6acc5525b0c7a9
Sergey Skorokhod <sergeyskorokhod2@gmail.com>
2023-07-02 02:52:12 +0300

    "await websocket.accept()" moved above to fix connection problems

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -10,20 +10,21 @@ router: APIRouter = APIRouter()
 @router.websocket("/ws")
 async def websocket_endpoint(access_token: str, websocket: WebSocket):
     try:
-        twitch_user = await get_user_info(access_token)
-    except Exception as e:
-        logger.error(e)
-        # await websocket.close(1000)
-        return
-
-    channel_name = twitch_user['login']
-
-    logger.info(f'connection request from user {twitch_user}')
-
-    bot = TwitchBot(token=access_token, channel=channel_name, websocket=websocket)
-
-    try:
         await websocket.accept()
+
+        try:
+            twitch_user = await get_user_info(access_token)
+        except Exception as e:
+            logger.error(e)
+            # await websocket.close(1000)
+            return
+
+        channel_name = twitch_user['login']
+
+        logger.info(f'connection request from user {twitch_user}')
+
+        bot = TwitchBot(token=access_token, channel=channel_name, websocket=websocket)
+
         await bot.start()
         while True:
             await websocket.receive()
